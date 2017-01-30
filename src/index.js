9e4b04e3f06e5edd9bb89404da766f41c0237fd9
Kim Wilson <k.wilson541@gmail.com>
2017-01-30 14:17:44 +0000

    Added files needed for CommentBox App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './CommentBox';
 
 ReactDOM.render(
-  <App />,
+  <CommentBox />,
   document.getElementById('root')
 );
